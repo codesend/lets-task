564a34e08802c4c81d2639334d9b5ed5b7aa48d8
Daniel Krause <dkrause1@myseneca.ca>
2023-02-28 23:52:02 -0500

    task router and controller & all task methods

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,19 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
 
-
-const app = express()
+const app = express();
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use("/api/tasks", taskRoutes);
 
 //Routes
 app.get("/", (req, res) => {
     res.send("Home page");
 });
-
-//Create a task
-app.post("/api/tasks", async (req, res) => {
-    try {
-    const task = await Task.create(req.body)
-    res.status(200).json(task)
-    } catch (error) {
-    res.status(500).json({msg: error.message})
-    }
-})
 
 const PORT = process.env.PORT || 5000
 
