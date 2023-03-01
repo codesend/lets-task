6c2402b98a7be21b19a7a322dca51e9d555dea5e
Daniel Krause <dkrause1@myseneca.ca>
2023-03-01 14:29:56 -0500

    save task to db

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,11 +1,39 @@
-import Task from "./Task"
-import TaskForm from "./TaskForm"
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
+import axios from "axios";
 
 const TaskList = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        completed: "false"
+    })
+    const {name} = formData;
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target
+        setFormData({ ...formData, [name]: value })
+    };
+
+    const createTask = async (e) => {
+        e.preventDefault();
+        if (name === "") {
+            return toast.error("Input field cannot be empty")
+        }
+        try {
+            await axios.post("http://localhost:5000/api/tasks", formData);
+            toast.success("Task added");
+            setFormData({ ...formData, name: ""});
+        } catch (error) {
+            toast.error(error);
+        }
+    };
+
   return (
     <div>
-        <h2>Task List</h2>
-        <TaskForm />
+        <h2>Task Manager</h2>
+        <TaskForm name={name} handleInputChange={handleInputChange} createTask={createTask}/>
         <div className="--flex-between --pb">
             <p>
                 <b>Total Tasks:</b> 0
