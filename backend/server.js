87453c8f20010a2d142f7b01a880b129cfaea89d
Daniel Krause <dkrause1@myseneca.ca>
2023-02-28 21:05:27 -0500

    setup mongoose, dotenv, express

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+
+
+const app = express()
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+const PORT = process.env.PORT || 5000
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+startServer();
