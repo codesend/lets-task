66e20ef2d402f63caadaa65efba30c8ad29a4233
Daniel Krause <dkrause1@myseneca.ca>
2023-02-28 23:33:45 -0500

    setup task model and create a task

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,29 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const Task = require("./models/taskModel");
 
 
 const app = express()
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //Routes
 app.get("/", (req, res) => {
     res.send("Home page");
 });
+
+//Create a task
+app.post("/api/tasks", async (req, res) => {
+    try {
+    const task = await Task.create(req.body)
+    res.status(200).json(task)
+    } catch (error) {
+    res.status(500).json({msg: error.message})
+    }
+})
 
 const PORT = process.env.PORT || 5000
 
